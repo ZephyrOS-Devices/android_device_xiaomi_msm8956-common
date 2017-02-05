ed311aa248709fac8b0d3407427949e2e7c2ea54
Bitti09 <bitti06@gmail.com>
2017-02-05 12:04:46 +0100

    a/v tests

--- msm8956.mk
+++ msm8956.mk
@@ -190,7 +190,9 @@ PRODUCT_PACKAGES += \
     libOmxCore \
     libOmxEvrcEnc \
     libOmxQcelp13Enc \
+    libOmxSwVencMpeg4 \
     libOmxSwVencHevc \
+    libOmxVidcCommon \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw
@@ -238,6 +240,7 @@ PRODUCT_PACKAGES += \
     libqsap_sdk \
     libQWiFiSoftApCfg \
     libwpa_client \
+    libwifi-hal-qcom \
     hostapd \
     wpa_supplicant \
     wpa_supplicant.conf
